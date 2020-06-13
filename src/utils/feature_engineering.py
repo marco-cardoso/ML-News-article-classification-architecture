0879f4d30182822449679029a6f2c1acf6a1b6aa
Marco Cardoso <marcom.c@outlook.com>
2020-06-13 06:32:51 +0100

    Update EDA notebook

--- src/utils/feature_engineering.py
+++ src/utils/feature_engineering.py
@@ -6,11 +6,12 @@ nltk.download('stopwords')
 nltk.download('wordnet')
 STOPWORDS = nltk.corpus.stopwords.words('english')
 
+tokenizer = nltk.RegexpTokenizer(r'\w+')
+lemmatizer = nltk.stem.WordNetLemmatizer()
 
 def preprocess(df: pd.DataFrame) -> pd.DataFrame:
     text_columns = ['content', 'title']
-    tokenizer = nltk.RegexpTokenizer(r'\w+')
-    lemmatizer = nltk.stem.WordNetLemmatizer()
+    
 
     for column in text_columns:
         # Changing to lower case and removing punctuation
@@ -25,4 +26,9 @@ def preprocess(df: pd.DataFrame) -> pd.DataFrame:
         # Lemmatizing the corpus
         df[column] = df[column].apply(lambda x: [lemmatizer.lemmatize(word) for word in x])
 
+    df['topics'] = df['topics'].apply(preprocess_topics)
     return df
+
+def preprocess_topics(topics):
+    topics = [ lemmatizer.lemmatize(item.lower()) for item in topics if item not in STOPWORDS]
+    return topics
