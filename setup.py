46ee53be8ac22620ec2cacdc06702a9614735540
Marco Cardoso <marcom.c@outlook.com>
2020-06-15 09:53:20 +0100

    Organize project

--- setup.py
+++ setup.py
@@ -1,13 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+    Setup file for news_classifier.
+    Use setup.cfg to configure your project.
+
+    This file was generated with PyScaffold 3.2.3.
+    PyScaffold helps you to put up the scaffold of your new Python project.
+    Learn more under: https://pyscaffold.org/
+"""
+import sys
+
+from pkg_resources import VersionConflict, require
 from setuptools import setup
 
-setup(
-    name='news-classifier',
-    version='0.01',
-    packages=[''],
-    package_dir={'': ''},
-    url='https://github.com/marco-cardoso/news-classifier',
-    license='',
-    author='Marco Cardoso',
-    author_email='marcom.c@outlook.com',
-    description='Necessary scripts to classify news articles'
-)
+try:
+    require('setuptools>=38.3')
+except VersionConflict:
+    print("Error: version of setuptools is too old (<38.3)!")
+    sys.exit(1)
+
+
+if __name__ == "__main__":
+    setup(use_pyscaffold=True)
