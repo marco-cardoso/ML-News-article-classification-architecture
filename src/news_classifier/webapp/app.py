c56ceeac08640958ce2ab4c8d21242c714b28c52
Marco Cardoso <marcom.c@outlook.com>
2020-07-08 21:26:41 +0100

    HTML form to submit the news article

--- src/news_classifier/webapp/app.py
+++ src/news_classifier/webapp/app.py
@@ -1,28 +1,34 @@
 import numpy as np
 
-from flask import Flask, request
+from flask import Flask, request, render_template, redirect, session
 
 from news_classifier.models import predict
 
 
 app = Flask(__name__)
+app.secret_key = "}@$/=(+@;7`9~8/5"
 
 
 @app.route("/")
 def index():
-    return "News classifier webapp"
+    return render_template("index.html")
 
 
 @app.route("/classify", methods=['POST'])
 def classify_news():
     if request.method == 'POST':
         article_text = request.form['text']
+
+        session['news-article'] = article_text
+
         category = predict(np.array(
             [
                 article_text
             ]
-        ))
-        return category[0]
+        ))[0]
+
+        session['category'] = category
+        return redirect("/")
 
 
 if __name__ == "__main__":
