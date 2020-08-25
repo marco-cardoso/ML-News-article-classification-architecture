a5904e35c165265367c8ee630a4e6d817a011f84
Marco Cardoso <marcom.c@outlook.com>
2020-08-25 16:22:36 -0700

    Create tests

---
+++ packages/news_classifier/tests/classifier.py
@@ -0,0 +1,14 @@
+from news_classifier.models.predict import predict
+from news_classifier.utils import text_examples
+
+
+def prediction():
+    data = {
+        'content': text_examples.business
+    }
+    result = predict(data)
+
+    assert result is not None
+    assert isinstance(result.get('prediction'), str)
+    assert result.get("prediction") == "business"
+
