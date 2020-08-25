ecc37727a43786cf28a63e3a961ddcc1878f3018
Marco Cardoso <marcom.c@outlook.com>
2020-08-25 14:35:53 -0700

    Update model_management.py
    Load pipeline method

--- packages/news_classifier/news_classifier/utils/model_management.py
+++ packages/news_classifier/news_classifier/utils/model_management.py
@@ -6,11 +6,25 @@ from news_classifier.config import paths
 from news_classifier.version import __version__
 
 
+def _get_latest_model_path() -> str:
+    save_file_name = f"{paths.PIPELINE_SAVE_FILE}{__version__}.pkl"
+    save_path = paths.MODELS_DIR / save_file_name
+    return save_path
+
+
 def save_pipeline(pipeline: Pipeline) -> None:
     """
     Store a pipeline
     :param pipeline: A scikit-learn pipeline
     """
-    save_file_name = f"{paths.PIPELINE_SAVE_FILE}{__version__}.pkl"
-    save_path = paths.MODELS_DIR / save_file_name
+    save_path = _get_latest_model_path()
     joblib.dump(pipeline, save_path)
+
+
+def load_pipeline() -> Pipeline:
+    """
+    Load the latest pipeline
+    """
+    load_path = _get_latest_model_path()
+    model = joblib.load(load_path)
+    return model
