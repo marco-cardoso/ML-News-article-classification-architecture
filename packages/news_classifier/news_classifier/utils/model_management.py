6a3e162d563fbb54ea2a907137e7f89714253d77
Marco Cardoso <marcom.c@outlook.com>
2020-09-02 15:20:06 -0700

    Update model_management.py
    Use timestamp as the model name

--- packages/news_classifier/news_classifier/utils/model_management.py
+++ packages/news_classifier/news_classifier/utils/model_management.py
@@ -2,19 +2,25 @@ import joblib
 from sklearn.pipeline import Pipeline
 
 from news_classifier.config import paths
-
+from news_classifier.database import db
 from news_classifier.version import __version__
 
 
 def _get_latest_model_path() -> str:
-    save_file_name = f"{paths.PIPELINE_SAVE_FILE}{__version__}.pkl"
+    latest_article_date = db.get_last_article_date()
+    ad_timestamp = latest_article_date.timestamp()
+
+    pipeline_name = paths.PIPELINE_NAME
+    pipeline_version = __version__
+
+    save_file_name = f"{ad_timestamp}.pkl"
     save_path = paths.MODELS_DIR / save_file_name
     return save_path
 
 
 def save_pipeline(pipeline: Pipeline) -> None:
     """
-    Store a pipeline
+    Store a pipeline locally
     :param pipeline: A scikit-learn pipeline
     """
     save_path = _get_latest_model_path()
