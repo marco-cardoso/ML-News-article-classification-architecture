38c261a26c61324458cd59d79e57d5889c4686dd
Marco Cardoso <marcom.c@outlook.com>
2020-09-03 09:44:34 -0700

    Ceate evaluation module

---
+++ packages/news_classifier/news_classifier/models/evaluate.py
@@ -0,0 +1,64 @@
+import logging
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import KFold
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
+
+_logger = logging.getLogger(__name__)
+
+
+def get_overall_results(results):
+    overall_results = {}
+
+    df = pd.DataFrame.from_dict(results, orient="index")
+    for metric in df.columns:
+        overall_results[metric + "_mean"] = df[metric].mean()
+        overall_results[metric + "_std"] = df[metric].std()
+
+    return overall_results
+
+
+def calculate_efficiency_metrics(pipeline, X, y):
+    """
+    Apply KFold to the given data and calculate the follow metrics for each fold :
+        - accuracy
+        - precision
+        - recall
+        - f1-score
+        - confusion matrix
+
+    Finally, it will calculate for each metric its mean and standard deviation.
+    :param pipeline: A Sklearn pipeline
+    :param X: A numpy array with the features
+    :param y: A numpy array with the target
+    """
+    _logger.info("Calculating efficiency metrics")
+
+    kfold = KFold(n_splits=5, random_state=4532, shuffle=True)
+    results = {}
+    confusion_matrices = []
+
+    fold_number = 0
+    for train_index, test_index in kfold.split(X):
+        X_train, X_test = X.iloc[train_index], X.iloc[test_index]
+        y_train, y_test = y.iloc[train_index], y.iloc[test_index]
+
+        pipeline.fit(X_train, y_train)
+
+        y_pred = pipeline.predict(X_test)
+
+        metrics = {
+            'accuracy': accuracy_score(y_test, y_pred),
+            'precision': precision_score(y_test, y_pred, average="weighted", labels=np.unique(y_pred)),
+            'recall': recall_score(y_test, y_pred, average="weighted", labels=np.unique(y_pred)),
+            'f1': f1_score(y_test, y_pred, average="weighted", labels=np.unique(y_pred))
+        }
+
+        cm = confusion_matrix(y_test, y_pred)
+        confusion_matrices.append(cm)
+
+        results[fold_number] = metrics
+        fold_number += 1
+
+    overall_results = get_overall_results(results)
+    _logger.info("Metrics generated successfully")
--- packages/news_classifier/news_classifier/models/train_model.py
+++ packages/news_classifier/news_classifier/models/train_model.py
@@ -3,6 +3,7 @@ import logging
 from news_classifier.config import variables
 from news_classifier.models.pipeline import category_classifier
 from news_classifier.utils import load_data, model_management
+from news_classifier.models.evaluate import calculate_efficiency_metrics
 
 _logger = logging.getLogger(__name__)
 
@@ -12,6 +13,12 @@ def train():
     df = load_data()
 
     X, y = df[variables.FEATURES], df[variables.TARGET]
+
+    # TODO Calculate metrics using KFold
+    # TODO Save metrics using MLFlow
+    calculate_efficiency_metrics(category_classifier, X, y)
+    # TODO Save the confusion matrix plot somewhere
+    # TODO Train the model with all the data available
 
     _logger.info("Training the model")
     category_classifier.fit(X, y)
