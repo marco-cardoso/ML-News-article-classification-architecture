3f27a78fde40e558026825a6d69a10eecbd48ea6
Marco Cardoso <marcom.c@outlook.com>
2020-09-02 23:00:00 -0700

    Update predict.py

--- packages/news_classifier/news_classifier/models/predict.py
+++ packages/news_classifier/news_classifier/models/predict.py
@@ -1,23 +1,32 @@
-import typing as t
 import logging
+import typing as t
 
 import pandas as pd
 
-from news_classifier.utils.model_management import load_pipeline
 from news_classifier.utils import text_examples
-from news_classifier.version import __version__
+from news_classifier.utils.model_management import load_pipeline, update_model
 
+# It's necessary to load the model here otherwise
+# at each prediction the model will be
+# reloaded increasing the processing time
 pipeline = load_pipeline()
 
 
 def predict(input_data: t.Union[pd.DataFrame, dict]) -> dict:
+    global pipeline
+
     _logger = logging.getLogger(__name__)
+
+    # This method checks if there's a new model
+    # available and then updates it. Otherwise, it keeps
+    # the same
+    pipeline = update_model(pipeline)
 
     _logger.info("Generating a prediction.")
     data = pd.DataFrame([input_data])
 
     predictions = pipeline.predict(data)
-    results = {"prediction": predictions[0], "version": __version__}
+    results = {"prediction": predictions[0]}
     return results
 
 
