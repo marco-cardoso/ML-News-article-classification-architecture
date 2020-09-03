77da055180913d14df0bc2f3f95ccfa5414fcdd6
Marco Cardoso <marcom.c@outlook.com>
2020-09-03 15:11:20 -0700

    Update train_model.py
    Add MLFlow

--- packages/news_classifier/news_classifier/models/train_model.py
+++ packages/news_classifier/news_classifier/models/train_model.py
@@ -1,11 +1,17 @@
+import os
 import logging
 
-from news_classifier.config import variables
+from news_classifier.config import variables, paths
 from news_classifier.models.pipeline import category_classifier
 from news_classifier.utils import load_data, model_management
 from news_classifier.models.evaluate import calculate_efficiency_metrics
 
+import mlflow
+
 _logger = logging.getLogger(__name__)
+
+mlflow.set_tracking_uri(f"http://{os.environ['MLFLOW_HOST']}:{os.environ['MLFLOW_PORT']}")
+mlflow.set_experiment(paths.PIPELINE_NAME)
 
 
 def train():
@@ -14,11 +20,7 @@ def train():
 
     X, y = df[variables.FEATURES], df[variables.TARGET]
 
-    # TODO Calculate metrics using KFold
-    # TODO Save metrics using MLFlow
     calculate_efficiency_metrics(category_classifier, X, y)
-    # TODO Save the confusion matrix plot somewhere
-    # TODO Train the model with all the data available
 
     _logger.info("Training the model")
     category_classifier.fit(X, y)
