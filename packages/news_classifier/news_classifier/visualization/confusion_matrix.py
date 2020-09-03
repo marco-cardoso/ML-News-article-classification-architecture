10e052a4bd27c47d779a701891400df457b49bc1
Marco Cardoso <marcom.c@outlook.com>
2020-09-03 15:11:38 -0700

    Update confusion_matrix.py
    Option to save the confusion matrix

--- packages/news_classifier/news_classifier/visualization/confusion_matrix.py
+++ packages/news_classifier/news_classifier/visualization/confusion_matrix.py
@@ -1,9 +1,14 @@
+import logging
 import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix
 
+from news_classifier.config import paths
 
-def plot(y_true, y_pred, xticklabels: list, yticklabels: list, title: str):
+_logger = logging.getLogger(__name__)
+
+
+def plot(y_true, y_pred, xticklabels: list, yticklabels: list, title: str, to_save: bool):
     """
     It plots a confusion matrix using the seaborn heatmap function
     :param y_true: A List with the Y values
@@ -11,10 +16,17 @@ def plot(y_true, y_pred, xticklabels: list, yticklabels: list, title: str):
     :param xticklabels: A list with the xticklabels values
     :param yticklabels: A list with the yticklabels values
     :param title: A String with the estimator title
+    :param to_save: A boolean to save the plot on disk
+    :return: If the parameter save equals to true then it saves the plot
+    on disk and returns its path
     """
     cm = confusion_matrix(y_true, y_pred)
 
-    fig, ax = plt.subplots(figsize=(10, 10))
+    if to_save:
+        fig, ax = plt.subplots(figsize=(4, 4))
+    else:
+        fig, ax = plt.subplots(figsize=(10, 10))
+
     sns.heatmap(
         cm,
         annot=True,
@@ -26,4 +38,10 @@ def plot(y_true, y_pred, xticklabels: list, yticklabels: list, title: str):
     plt.ylabel("Real")
     plt.title(f"{title} confusion matrix")
 
-    plt.show()
+    if not to_save:
+        plt.show()
+    else:
+        _logger.info(f"Storing {title} on disk")
+        path = paths.MODEL_PLOTS_DIR / (title + ".jpg")
+        plt.savefig(path)
+        return path
