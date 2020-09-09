cb17466c2b1afaf935deb4f15c4250977ed37c1e
Marco Cardoso <marcom.c@outlook.com>
2020-09-08 22:51:02 -0700

    Update paths.py
    Create the MODELS directory if it does not exist

--- packages/news_classifier/news_classifier/config/paths.py
+++ packages/news_classifier/news_classifier/config/paths.py
@@ -1,12 +1,21 @@
 """
     Module to store the path settings
 """
+import logging
 import pathlib
 
 import news_classifier
 
+_logger = logging.getLogger(__name__)
+
 PACKAGE_ROOT = pathlib.Path(news_classifier.__file__).resolve().parent
-MODELS_DIR = PACKAGE_ROOT / "models" / "models"
+MODELS_DIR = pathlib.Path("/models").absolute()
+
+if not MODELS_DIR.exists():
+    _logger.info(f"Creating {MODELS_DIR}")
+    MODELS_DIR.mkdir()
+
+MODEL_PLOTS_DIR = MODELS_DIR / "plots"
 
 PIPELINE_NAME = "linear_svc"
 
