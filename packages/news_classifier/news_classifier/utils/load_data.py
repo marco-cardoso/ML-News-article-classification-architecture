d38e7902d2a1fb626664b948cb2cd85f61e9ff7d
Marco Cardoso <marcom.c@outlook.com>
2020-09-08 22:55:55 -0700

    Update load_data.py

--- packages/news_classifier/news_classifier/utils/load_data.py
+++ packages/news_classifier/news_classifier/utils/load_data.py
@@ -3,12 +3,14 @@ import pandas as pd
 from news_classifier.database import db
 
 
-def load_data() -> pd.DataFrame:
+def load_data(projection: dict) -> pd.DataFrame:
     """
     Load the data from the Mongo collection and transform
     into a pandas dataframe
+    :projection: A dictionary with the fields to load from database
     :return: A pandas dataframe with the data
     """
-    articles = db.read_articles()
+    articles = db.read_articles(
+        projection=projection
+    )
     return pd.DataFrame(articles)
-
